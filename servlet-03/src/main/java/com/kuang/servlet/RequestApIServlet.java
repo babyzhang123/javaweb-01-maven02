6926470c8499bc33b72a96ae31d7dd99fc743ab1
Ellie <414514278@qq.com>
2021-07-10 15:49:29 +0800

    first

---
+++ servlet-03/src/main/java/com/kuang/servlet/RequestApIServlet.java
@@ -0,0 +1,21 @@
+package com.kuang.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RequestApIServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(req.getRequestURI());
+        System.out.println(req.getRequestURL());
+        System.out.println(req.getRemoteHost());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
